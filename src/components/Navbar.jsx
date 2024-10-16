3e666909019a32f36b146aac22fb0cc04506512d
Chris <cristru8@gmail.com>
2024-10-16 16:38:29 -0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        {/* todo: ADD LOGO */}
+        <h1 className="text-4xl font-semibold">Chris</h1>
+      </div>
+      <div>
+        {/* todo: ADD Buttons */}
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
